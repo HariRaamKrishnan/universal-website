9555117aa601bfa191a3378a9e69e67156aead3b
“HariRaamKrishnan” <https://github.com/HariRaamKrishnan/universal-website.git>
2023-09-02 20:52:06 +0530

    i am just add some code

--- content/blog/hello.py
+++ content/blog/hello.py
@@ -3,3 +3,15 @@
 #krishnan
 # how are youfsdafsda
 print("hari")
+if True:
+print("true")
+print("done")
+else:
+print("false")
+print("Task is Done")  def addFunction():
+print("Hi")
+
+if True:
+print("true")
+else:
+print("false")
